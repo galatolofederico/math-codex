81c5456bf497c1059d7e3c797cfb738ce8d66ae7
Federico Galatolo <galatolo.federico@gmail.com>
2021-12-13 18:34:02 +0100

    wrote a thread-safe subporcess version of execute_program

--- src/execute.py
+++ src/execute.py
@@ -3,7 +3,12 @@ import ast
 import signal
 import io
 
-def execute_program(program):
+import os
+import tempfile
+from subprocess import check_output
+
+# (exec version) Fast but not thread-safe execute_program 
+def execute_program_fast(program):
     lines = program.split("\n")
     lines[-1] = "output = "+lines[-1]+"\nprint(output)"
     program = "\n".join(lines)
@@ -42,4 +47,37 @@ def execute_program(program):
         halts=program_halts,
         error=program_error,
         output=program_output.getvalue().strip()
+    )
+
+
+# (subprocess version) Slower but thread-safe execute_program
+def execute_program_subprocess(program):
+    lines = program.split("\n")
+    lines[-1] = "output = "+lines[-1]+"\nprint(output)"
+    program = "\n".join(lines)
+
+    program_output = io.StringIO()
+    valid_program = True
+    program_halts = False
+    program_error = False
+
+    try:
+        code = ast.parse(program, mode="exec")
+    except:
+        valid_program = False
+    
+    if valid_program:
+        fd, fname = tempfile.mkstemp()
+
+        with os.fdopen(fd, "w") as tmp:
+            tmp.write(program)
+        program_output = check_output(["python3", fname], timeout=2)
+
+        os.remove(fname)
+
+    return dict(
+        valid=valid_program,
+        halts=program_halts,
+        error=program_error,
+        output=program_output.decode("utf-8")
     )
